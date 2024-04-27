b93d821404698ca945310db635967497436cfb23
afrguilherme <augustogafr@gmail.com>
2024-04-27 18:08:30 -0300

    Properties table migration added

---
+++ src/database/migrations/20240427202620-create-properties-table.js
@@ -0,0 +1,79 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('properties', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      town_house: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dimensions: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rooms: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      parking_space: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      bathrooms: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      contact: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('properties')
+  },
+}
