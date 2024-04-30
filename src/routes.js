022f701f82268f08e5e34d7c6962dd02f02cf55c
afrguilherme <augustogafr@gmail.com>
2024-04-29 21:31:46 -0300

    Property images upload added

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import PropertyController from './app/controllers/PropertyController'
 
 const routes = new Router()
 
+const upload = multer(multerConfig)
+
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
-routes.post('/properties', PropertyController.store)
+routes.post('/properties', upload.array('files', 4), PropertyController.store)
 
 export default routes
